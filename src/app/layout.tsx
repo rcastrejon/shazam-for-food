20c5d57ff242459a9fdeb67ebcd6e067f5ecd286
Rodrigo Castrejon <rodrigocastrejonc01@gmail.com>
2024-07-14 12:48:26 -0600

    initialize shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { cn } from "~/lib/utils";
+
 import "./globals.css";
 
 const inter = Inter({
@@ -20,8 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body>{children}</body>
+    <html lang="en">
+      <body
+        className={cn("bg-background font-sans antialiased", inter.variable)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1>Hello, world!</h1>
+      <div className="flex flex-col gap-2">
+        <h1>Hello, world!</h1>
+        <Button className="mx-auto">Noop</Button>
+      </div>
     </main>
   );
 }
