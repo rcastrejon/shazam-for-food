5f057ca7b95bf09bf4a0cc7a79a8a01dc3be603d
Rodrigo Castrejon <rodrigocastrejonc01@gmail.com>
2024-07-17 12:23:43 -0600

    refactor: improve the component composition of the GenerativeArea & improve llm prompt performance

--- src/app/_ai/actions.tsx
+++ src/app/_ai/actions.tsx
@@ -2,6 +2,7 @@
 
 import { streamObject } from "ai";
 import { createStreamableUI } from "ai/rsc";
+import { Loader } from "lucide-react";
 import { ollama } from "ollama-ai-provider";
 import { z } from "zod";
 
@@ -9,22 +10,29 @@ function Thinking({ thoughts }: { thoughts: string | undefined }) {
   if (thoughts) {
     return <div className="text-muted-foreground">{thoughts}</div>;
   }
-  return <div className="text-muted-foreground">Thinking...</div>;
+  return <Loader className="h-4 w-4 animate-spin text-muted-foreground" />;
 }
 
-export async function streamComponent({ image }: { image: string }) {
+export async function streamFirstAnalysisComponent({
+  image,
+}: {
+  image: string;
+}) {
   "use server";
 
   const stream = createStreamableUI(<Thinking thoughts={undefined} />);
 
   void (async () => {
     const { partialObjectStream } = await streamObject({
-      model: ollama("llava-phi3"), // TODO: Change to the final model
-      system: `
-You have perfect vision and pay great attention to detail which makes you an \
-expert at identifying ingredients in food. You are tasked with figuring out \
-the exact ingredients shown in the picture and give the dish an appropriate \
-name.`,
+      model: ollama("llava"), // TODO: Change to the final model
+      system: `You have perfect vision and pay great attention to detail which \
+makes you an expert at identifying ingredients in food. You are tasked with \
+figuring out the exact ingredients shown in the picture and give the dish an \
+appropriate name.
+
+When thinking, be as descriptive as possible, consider every detail in the \
+image, discuss the portions, the harmony of flavours, and how certain are you \
+about each ingredient.`,
       messages: [
         {
           role: "user",
@@ -32,33 +40,33 @@ name.`,
             { type: "image", image },
             {
               type: "text",
-              text: `
-Tell me with certainty what ingredients are in the dish. To improve your \
-chances of figuring out the right ingredients, you will display a list of \
-words for the ingredients that you are not completely certain, sort of like a \
-word cloud. Then, I will select the words that correspond to the dish. The \
-idea is that you only list words that will help you identify the dish, so I \
-can discard the irrelevant words. Before providing the answer in 'words' \
-field, think step-by-step in 'thinking' field and analyze every part of the \
-image.`,
+              text: `What ingredients are in the dish? How certain are you \
+about each one of them? To clear up any uncertainties you have, I will help \
+you out. For that, you will give me a list of checkboxes I can choose from. \
+So, for every uncertainty, you will give me different options and I will \
+select the ones that best apply to the dish. You can safely assume that any \
+option that I don't choose, does not apply to the dish. Before providing the \
+answer in 'checkboxes' field, think step-by-step in 'thinking' field and \
+analyze every part of the image.`,
             },
           ],
         },
       ],
       schema: z.object({
         thinking: z.string(),
-        words: z.array(z.string()),
+        isFood: z.boolean(),
+        checkboxes: z.array(z.string()),
       }),
     });
 
     for await (const partialObject of partialObjectStream) {
-      if (!partialObject.words) {
+      if (!partialObject.checkboxes) {
         stream.update(<Thinking thoughts={partialObject.thinking} />);
         continue;
       }
       stream.update(
         <div>
-          {partialObject.words.map((word, i) => (
+          {partialObject.checkboxes.map((word, i) => (
             <div key={i}>{word}</div>
           ))}
         </div>,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 import { Button } from "~/components/ui/button";
@@ -10,7 +10,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
-import { cn } from "~/lib/utils";
+import { streamFirstAnalysisComponent } from "./_ai/actions";
 import {
   GenerativeAreaProvider,
   useGenerativeArea,
@@ -19,26 +19,49 @@ import {
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="mx-auto w-full max-w-screen-sm">
+      <div className="mx-auto w-full max-w-screen-lg">
         <GenerativeAreaProvider>
-          <GenerativeArea />
+          <GenerativeArea>
+            {(picture, onImageInputChange, onRetakePicture) => (
+              <div className="px-6 py-8">
+                <Viewfinder
+                  picture={picture}
+                  onImageInputChange={onImageInputChange}
+                />
+                <Controls
+                  picture={picture}
+                  onImageInputChange={onImageInputChange}
+                  onRetakePicture={onRetakePicture}
+                />
+              </div>
+            )}
+          </GenerativeArea>
         </GenerativeAreaProvider>
       </div>
     </div>
   );
 }
 
-function GenerativeArea() {
-  const { state, dispatch } = useGenerativeArea(); // TODO: Context may not be necessary
+type GenerativeAreaProps = {
+  children: (
+    picture: string | null,
+    onImageInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    onRetakePicture: () => void,
+  ) => React.ReactNode;
+};
 
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+function GenerativeArea({ children }: GenerativeAreaProps) {
+  const { state, dispatch } = useGenerativeArea(); // TODO: Context may not be necessary
+  const { picture } = state;
+
+  function onImageInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     const file = e.target.files?.[0];
     if (!file?.type.startsWith("image/")) {
-      return;
+      throw new Error("Invalid file type");
     }
 
     const reader = new FileReader();
-    reader.onload = async () => {
+    reader.onload = () => {
       dispatch({
         type: "SET_PICTURE",
         payload: reader.result as string,
@@ -47,72 +70,68 @@ function GenerativeArea() {
     reader.readAsDataURL(file);
   }
 
-  function handleRetakePicture() {
+  function onRetakePicture() {
     dispatch({
       type: "UNSET_PICTURE",
     });
   }
 
-  function handleStartAnalysis() {
-    console.log("start analysis");
-  }
-
-  return (
-    <div className="px-6 py-8">
-      <Viewfinder picture={state.picture} onInputChange={handleInputChange} />
-      <ViewfinderControls
-        isPictureLoaded={state.picture !== null}
-        onInputChange={handleInputChange}
-        handleRetakePicture={handleRetakePicture}
-        handleStartAnalysis={handleStartAnalysis}
-      />
-    </div>
-  );
+  return children(picture, onImageInputChange, onRetakePicture);
 }
 
 type ViewfinderProps = {
   picture: string | null;
-  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onImageInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-function Viewfinder({ picture, onInputChange }: ViewfinderProps) {
+function Viewfinder({ picture, onImageInputChange }: ViewfinderProps) {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="relative mx-auto h-[270px] w-[270px] overflow-hidden rounded-3xl bg-muted">
-      <div
-        className={cn("grid h-full place-content-center", picture && "hidden")}
-      >
-        <input
-          className="hidden"
-          type="file"
-          accept="image/*"
-          capture="environment"
-          onChange={onInputChange}
-          ref={inputRef}
-        />
-        <TooltipProvider delayDuration={500}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                className="mx-auto"
-                onClick={() => inputRef.current?.click()}
-              >
-                Start camera
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>
-              <span className="text-xs">
-                Camera only available on mobile devices
-              </span>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
+      {!picture && (
+        <div className="absolute inset-0 grid place-content-center">
+          <input
+            className="hidden"
+            type="file"
+            accept="image/*"
+            capture="environment"
+            onChange={onImageInputChange}
+            ref={inputRef}
+          />
+          <TooltipProvider delayDuration={500}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  className="mx-auto"
+                  onClick={() => inputRef.current?.click()}
+                >
+                  Start camera
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className="text-xs">
+                  Camera only available on mobile devices
+                </span>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      )}
+
       <AnimatePresence>
         {picture && (
-          <div className="absolute left-0 top-0 h-full w-full">
-            <ViewfinderPicture picture={picture} />
+          <div className="absolute inset-0">
+            <motion.img
+              className="pointer-events-none h-full w-full touch-none object-cover"
+              initial={{ filter: "blur(20px)", scale: 1.5 }}
+              animate={{ filter: "blur(0px)", scale: 1 }}
+              exit={{ filter: "blur(20px)", scale: 1.5, opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+              src={picture}
+              alt="A user uploaded picture."
+              draggable={false}
+            />
           </div>
         )}
       </AnimatePresence>
@@ -120,65 +139,60 @@ function Viewfinder({ picture, onInputChange }: ViewfinderProps) {
   );
 }
 
-function ViewfinderPicture({ picture }: { picture: string }) {
-  return (
-    <motion.img
-      className="pointer-events-none touch-none object-cover"
-      initial={{ filter: "blur(20px)", scale: 1.5 }}
-      animate={{ filter: "blur(0px)", scale: 1 }}
-      exit={{ filter: "blur(20px)", scale: 1.5, opacity: 0 }}
-      transition={{ duration: 0.3, ease: "easeOut" }}
-      src={picture}
-      alt="A user uploaded picture."
-      draggable={false}
-    />
-  );
-}
-
-type ViewfinderControlsProps = {
-  isPictureLoaded: boolean;
-  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleRetakePicture: () => void;
-  handleStartAnalysis: () => void;
+type ControlsProps = {
+  picture: string | null;
+  onImageInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onRetakePicture: () => void;
 };
 
-function ViewfinderControls({
-  isPictureLoaded,
-  onInputChange,
-  handleRetakePicture,
-  handleStartAnalysis,
-}: ViewfinderControlsProps) {
+function Controls({
+  picture,
+  onImageInputChange,
+  onRetakePicture,
+}: ControlsProps) {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [genUI, setGenUI] = useState<React.ReactNode | null>(null);
 
-  if (isPictureLoaded) {
+  async function handleStartAnalysis() {
+    if (!picture) {
+      throw new Error("No picture to analyze");
+    }
+    setGenUI(await streamFirstAnalysisComponent({ image: picture }));
+  }
+
+  if (genUI) {
+    return genUI;
+  }
+
+  if (picture === null) {
     return (
-      <div className="mx-auto grid max-w-fit gap-2 pt-4">
-        <Button size="sm" onClick={handleStartAnalysis}>
-          Continue
-        </Button>
-        <Button size="sm" variant="outline" onClick={handleRetakePicture}>
-          Retake picture
+      <div className="flex flex-col items-center gap-2 pt-2">
+        <span className="text-xs text-muted-foreground">or</span>
+        <input
+          className="hidden"
+          type="file"
+          accept="image/*"
+          onChange={onImageInputChange}
+          ref={inputRef}
+        />
+        <Button
+          onClick={() => inputRef.current?.click()}
+          size="sm"
+          variant="secondary"
+        >
+          Upload from camera roll
         </Button>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center gap-2 pt-2">
-      <span className="text-xs text-muted-foreground">or</span>
-      <input
-        className="hidden"
-        type="file"
-        accept="image/*"
-        onChange={onInputChange}
-        ref={inputRef}
-      />
-      <Button
-        onClick={() => inputRef.current?.click()}
-        size="sm"
-        variant="secondary"
-      >
-        Upload from camera roll
+    <div className="mx-auto grid max-w-fit gap-2 pt-4">
+      <Button size="sm" onClick={handleStartAnalysis}>
+        Continue
+      </Button>
+      <Button size="sm" variant="outline" onClick={onRetakePicture}>
+        Retake picture
       </Button>
     </div>
   );
